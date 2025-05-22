a779e1d95f427990da95ffd09cb56643f7a63397
maobaiqi <294519766@qq.com>
2025-05-22 18:45:25 +0800

    Update supabase.client.js

--- utils/supabase.client.js
+++ utils/supabase.client.js
@@ -1,13 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-// 针对 Vercel 环境的优化配置
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: false,  // Vercel 无状态环境需关闭
-    autoRefreshToken: false,
-    detectSessionInUrl: false // 防止 Vercel 重定向问题
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    auth: { persistSession: false }
   }
-})
+)
